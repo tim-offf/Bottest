a33777823f311f4863583adb323e9e30c4b53886
tim-offf <156789505+tim-offf@users.noreply.github.com>
2024-11-06 14:17:32 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-print('Da.')
+import tracemalloc
+import sys
+
+tracemalloc.start()
+
+full_list = [_ for _ in range(10_000)]
+snap = tracemalloc.take_snapshot()
+
+mini_list = [_ for _ in range(500)]
+mini_snap = tracemalloc.take_snapshot()
+
+for stat in snap.statistics('lineno'):
+    print(stat)
+
+for stat in mini_snap.compare_to(snap, 'lineno'):
+    print(stat)
+
+print(sys.getsizeof(mini_list))
+print(sys.getrefcount(full_list))
+
+print("Pizda blyat")
+#jjj
